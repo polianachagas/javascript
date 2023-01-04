6709203f2e1ab5c8a9836fbaf6b308cea665d92f
Poliana R. Chagas <polianarc934@gmail.com>
2023-01-04 14:36:20 -0300

    Initial commit

---
+++ Untitled-1.js
@@ -0,0 +1,12 @@
+// Write a message to the console.
+console.log('hello world!');
+console.log ('te amo momoi <3');
+
+let amor = 0;
+
+console.log('nível do amor do Rick');
+for (let index = 0; index < 1000000; index++) {
+    console.log (index);
+    let amor = 1/0;
+}
+
